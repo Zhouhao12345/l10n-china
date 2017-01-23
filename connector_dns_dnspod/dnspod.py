ee8b0010887189a355a8d947f9358b8cbc96cda3
noah <234082230@qq.com>
2017-01-23 14:37:33 +0800

    format

--- connector_dns_dnspod/dnspod.py
+++ connector_dns_dnspod/dnspod.py
@@ -162,7 +162,8 @@ def import_record(session, dns_domain_id, data):
                         record['value'])
             return 'Import record success %s' % record_list_str
         else:
-            return _('Import record error code: %s' % result_json['status']['code'])
+            return _('Import record error code: %s' %
+                   result_json['status']['code'])
     except:
         raise
 
